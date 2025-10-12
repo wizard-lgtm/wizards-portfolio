cd8801fc241fe7c141340cbded5208036b03deb5
wizard-lgtm <rippiten23@gmail.com>
2025-10-12 16:46:10 +0300

    feat: Create actix web server with a hello endpoint.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::env;
+use dotenv::dotenv; 
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Welcome to wizards-portfolio!")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok(); // Load .env file
+
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    println!("Application running on port {}", port);
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
